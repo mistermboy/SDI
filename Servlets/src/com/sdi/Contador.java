d09c07c540b932972eb988b744e5945b070c5066
mistermboy <pabloyo97@hotmail.com>
2018-02-11 20:06:28 +0100

    P1 with gitignore

---
+++ Servlets/src/com/sdi/Contador.java
@@ -0,0 +1,14 @@
+package com.sdi;
+
+public class Contador {
+	private int total;
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setIncremento(int incremento) {
+		total += incremento;
+	}
+
+}
