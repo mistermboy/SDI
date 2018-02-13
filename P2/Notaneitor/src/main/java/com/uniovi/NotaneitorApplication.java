c69c1884c657cd74ea0449730fc62aa020fb3abd
mistermboy <pabloyo97@hotmail.com>
2018-02-14 00:48:36 +0100

    P2

---
+++ P2/Notaneitor/src/main/java/com/uniovi/NotaneitorApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class NotaneitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaneitorApplication.class, args);
+	}
+}
---
+++ P2/Notaneitor/src/main/java/com/uniovi/controllers/MarksController.java
@@ -0,0 +1,50 @@
+package com.uniovi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.uniovi.entities.Mark;
+import com.uniovi.services.MarksService;
+
+@Controller
+public class MarksController {
+
+	@Autowired // Inyectar el servicio
+	private MarksService marksService;
+
+	@RequestMapping("/mark/list")
+	public String getList(Model model) {
+		model.addAttribute("markList", marksService.getMarks());
+		return "mark/list";
+	}
+
+	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
+	public String setMark(@ModelAttribute Mark mark) {
+		marksService.addMark(mark);
+		return "redirect:/mark/list";
+
+	}
+
+	@RequestMapping(value = "/mark/add")
+	public String getMark() {
+		return "mark/add";
+	}
+
+	@RequestMapping("/mark/details/{id}")
+	public String getDetail(Model model, @PathVariable Long id) {
+		model.addAttribute("mark", marksService.getMark(id));
+		return "mark/details";
+	}
+
+	@RequestMapping("/delete/{id}")
+	public String deleteMark(@PathVariable Long id) {
+		marksService.deleteMark(id);
+		return "redirect:/mark/list";
+	}
+
+}
