047eafca49251d789c73ac3d39921c424bbdeb20
mistermboy <pabloyo97@hotmail.com>
2018-02-20 18:48:25 +0100

    4 Autenticación y control de acceso

--- P3/Notaneitor/src/main/java/com/uniovi/controllers/UsersController.java
+++ P3/Notaneitor/src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,6 +1,8 @@
 package com.uniovi.controllers;
 
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -79,6 +81,10 @@ public class UsersController {
 
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
 	public String home(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		User activeUser = usersService.getUserByDni(dni);
+		model.addAttribute("markList", activeUser.getMarks());
 		return "home";
 	}
 }
