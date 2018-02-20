8160c300135ec95a0716462c03257ec96e8d197f
mistermboy <pabloyo97@hotmail.com>
2018-02-20 18:13:37 +0100

    3.3 Services

--- P3/Notaneitor/src/main/java/com/uniovi/entities/Mark.java
+++ P3/Notaneitor/src/main/java/com/uniovi/entities/Mark.java
@@ -3,6 +3,8 @@ package com.uniovi.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Mark {
@@ -13,6 +15,10 @@ public class Mark {
 	private String description;
 	private Double score;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Mark() {
 
 	}
@@ -22,6 +28,13 @@ public class Mark {
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 
 	public Long getId() {
@@ -46,6 +59,14 @@ public class Mark {
 
 	public void setScore(Double score) {
 		this.score = score;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	@Override
