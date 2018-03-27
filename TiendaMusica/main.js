23327a2d3e04a52d1bfea5dd39ebe827c72184b0
mistermboy <pabloyo97@hotmail.com>
2018-03-27 22:39:56 +0200

    P2NeoJS

---
+++ TiendaMusica/main.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+
+var expressSession = require('express-session');
+app.use(expressSession({
+ secret: 'abcdefg',
+ resave: true,
+ saveUninitialized: true
+}));
+
+var crypto = require('crypto');
+
+var fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
+var swig = require('swig');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+console.log("routerUsuarioSession");
+ if ( req.session.usuario ) {
+ // dejamos correr la petición
+ next();
+ } else {
+ console.log("va a : "+req.session.destino)
+ res.redirect("/identificarse");
+ }
+});
+// Aplicar routerUsuarioSession
+app.use("/canciones/agregar",routerUsuarioSession);
+app.use("/publicaciones",routerUsuarioSession);
+
+// routerAudios
+var routerAudios = express.Router();
+routerAudios.use(function(req, res, next) {
+	console.log("routerAudios");
+	var path = require('path');
+	var idCancion = path.basename(req.originalUrl, '.mp3');
+	gestorBD.obtenerCanciones( { _id : mongo.ObjectID(idCancion) }, function (canciones) {
+		if( canciones[0].autor == req.session.usuario ){
+			next();
+		} else {
+			res.redirect("/tienda");
+		}
+	})
+});
+
+// Aplicar routerAudios
+app.use("/audios/",routerAudios)
+
+app.use(express.static('public'));
+
+// Variables
+app.set('port', 8081);
+app.set('db','mongodb://admin:sdi@ds255768.mlab.com:55768/tiendamusica');
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
+
+
+
+// Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD); 
+
+// Lanzar servidor
+app.listen(app.get('port'), function() {
+	console.log("Servidor activo");
+});
